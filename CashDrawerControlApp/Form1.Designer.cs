efdee58991469e147f9b0725e2b7e680939a976b
Gohulan Somanathan <sgohul.24@gmail.com>
2024-03-19 11:48:23 +0530

    Add project files.

---
+++ CashDrawerControlApp/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CashDrawerControlApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBoxStatus = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 84);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Drawer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 84);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Get Status";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBoxStatus
+            // 
+            this.listBoxStatus.FormattingEnabled = true;
+            this.listBoxStatus.Location = new System.Drawing.Point(27, 52);
+            this.listBoxStatus.Name = "listBoxStatus";
+            this.listBoxStatus.Size = new System.Drawing.Size(188, 147);
+            this.listBoxStatus.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxStatus);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CashDrawerControlApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBoxStatus;
+    }
+}
+
---
+++ CashDrawerControlApp/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Security;
+
+namespace CashDrawerControlApp
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSerialPort();
+        }
+
+        private void InitializeSerialPort()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM4"; // Change this to match your actual COM port
+            _serialPort.BaudRate = 9600;   // Adjust baud rate if needed
+            
+            _serialPort.Open();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenCashDrawer();
+
+
+        }
+
+        private void OpenCashDrawer()
+        {
+            try
+            {
+                // Send a command to open the cash drawer
+                _serialPort.Write("Open"); 
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening cash drawer: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBoxStatus.Items.Clear(); // Clear the ListBox before updating
+
+            string[] portNames = SerialPort.GetPortNames(); // Get names of all available COM ports
+
+            foreach (string portName in portNames)
+            {
+                try
+                {
+                    using (SerialPort port = new SerialPort(portName))
+                    {
+                        port.Open(); // Attempt to open the port
+                        listBoxStatus.Items.Add($"Port {portName}: Open");
+                        port.Close(); // Close the port after checking
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    listBoxStatus.Items.Add($"Port {portName}: Access denied"); // Port is in use by another application
+                }
+                catch (Exception ex)
+                {
+                    listBoxStatus.Items.Add($"Port {portName}: {ex.Message}"); // Other exceptions
+                }
+            }
+        }
+
+    }
+}
+
